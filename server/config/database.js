c94030b1d909b1958344e278a589bd426509fb5b
TomboY90 <rjsahdia3@gmail.com>
2020-12-04 18:59:06 +0900

    fix db config

--- server/config/database.js
+++ server/config/database.js
@@ -3,5 +3,4 @@ module.exports = {
   user     : 'root',
   password : 'password1!@',
   database : 'study_db',
-  timezone: 'utc',
 };
--- server/middlewares/mysql-connect.js
+++ server/middlewares/mysql-connect.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql');
-
 // mysql Database info Object
 const dbconfig = require('../config/database');
+const mysql = require('mysql');
+
 // connect to mySQL
 const connection = mysql.createConnection(dbconfig);
 
